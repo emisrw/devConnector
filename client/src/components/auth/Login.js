e586226641b2f21266469e1a026f21a1e2382236
Darek Kilian <emis@MacBook-Pro.local>
2021-04-24 15:32:04 +0200

    Request test & login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,58 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 function Login() {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password) {
+      console.log("Passwords do not match");
+    } else {
+      console.log("Success");
+    }
+  };
+
+  return (
+    <>
+      <div class="alert alert-danger">Invalid credentials</div>
+      <h1 class="large text-primary">Sign In</h1>
+      <p class="lead">
+        <i class="fas fa-user"></i> Sign into Your Account
+      </p>
+      <form class="form" onSubmit={(e) => onSubmit(e)}>
+        <div class="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div class="form-group">
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => onChange(e)}
+            placeholder="Password"
+            name="password"
+          />
+        </div>
+        <input type="submit" class="btn btn-primary" value="Login" />
+      </form>
+      <p class="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </>
+  );
 }
 
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-
+import axios from "axios";
+import { Link } from "react-router-dom";
 const Register = () => {
   const [formData, setFormData] = useState({
     name: "",
@@ -12,12 +13,28 @@ const Register = () => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
       console.log("Passwords do not match");
     } else {
-      console.log(formData);
+      const newUser = {
+        name,
+        email,
+        password,
+      };
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        const body = JSON.stringify(newUser);
+        const res = await axios.post("/api/users", body, config);
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
   return (
@@ -76,7 +93,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </>
   );
